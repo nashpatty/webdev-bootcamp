70d0daadddf9bf0fe6db2ee2e5ecd6175925d730
nashpatty <m.malhan.niazi@gmail.com>
2021-01-30 14:58:10 -0500

    while loop

--- section-javascript/intro-js/app.js
+++ section-javascript/intro-js/app.js
@@ -1,11 +1,21 @@
 console.log('"first" js statement');
 
 // for loops
+console.log('for loops');
 for (let i = 0; i < 10; i++) {
   console.log(i + 1);
 }
 
 // counting down
+console.log('for loops - counting down');
 for (let i = 25; i >= 0; i -= 5) {
   console.log(i);
+}
+
+// while loop
+console.log('while loops');
+let count = 0;
+while (count < 10) {
+  count++;
+  console.log(count);
 }
