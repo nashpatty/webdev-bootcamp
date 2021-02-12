8da646ff7393e436cce982c23e41f64da4e2eb0c
nashpatty <m.malhan.niazi@gmail.com>
2021-02-12 13:24:35 -0500

    lexical scope

---
+++ section-21-functions-cont/scope/app.js
@@ -0,0 +1,13 @@
+// lexical scope
+function cryForHelp() {
+  const plea = 'help me!';
+  function offerPlea() {
+    console.log(plea);
+  }
+  offerPlea();
+}
+
+cryForHelp();
+
+// offer plea is not accessible
+// offerPlea();
