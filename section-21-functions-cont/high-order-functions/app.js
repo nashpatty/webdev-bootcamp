ccbb06673f99cc7d7728079b095f42c9bb824c75
nashpatty <m.malhan.niazi@gmail.com>
2021-02-12 13:43:59 -0500

    returning function as values

--- section-21-functions-cont/high-order-functions/app.js
+++ section-21-functions-cont/high-order-functions/app.js
@@ -1,3 +1,4 @@
+// passing functions as arguments
 function rollTwice(rollDie) {
   rollDie();
   rollDie();
@@ -9,3 +10,26 @@ function rollDie() {
 }
 
 rollTwice(rollDie);
+
+// returning functions as values
+function makeMysteryFunc() {
+  const rand = Math.random();
+  if (rand > 5) {
+    return function () {
+      console.log(':)');
+    };
+  }
+  return function () {
+    console.log(':(');
+  };
+}
+
+function makeBetweenFunc(min, max) {
+  return function (num) {
+    return num >= min && num <= max;
+  };
+}
+
+function isBetween(num) {
+  return num >= 50 && num <= 100;
+}
