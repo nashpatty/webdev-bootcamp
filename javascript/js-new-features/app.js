5536666db66f140aa018eb96f3384a5e113967ce
nashpatty <m.malhan.niazi@gmail.com>
2021-02-14 18:45:20 -0500

    array spread

--- javascript/js-new-features/app.js
+++ javascript/js-new-features/app.js
@@ -5,3 +5,14 @@ function rollDie(sides = 6) {
 }
 console.log('roll d4: ' + rollDie(4));
 console.log('roll default (d6): ' + rollDie());
+
+// ********************************************
+// spread
+const numbers = [1, 2, 3, 4, 53, 342, 234, 112, 3455];
+
+// this will return NaN because max() expects numbers as arguments
+console.log(Math.max(numbers));
+
+// this will find the right max, with the spread syntax
+// passes each element in the array as a separate arguments
+console.log(Math.max(...numbers));
