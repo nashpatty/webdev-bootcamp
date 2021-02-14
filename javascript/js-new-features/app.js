df5e425fb359604ffb9ab668aa3a222d0adb275b
nashpatty <m.malhan.niazi@gmail.com>
2021-02-14 18:49:49 -0500

    using spread to combine arrays

--- javascript/js-new-features/app.js
+++ javascript/js-new-features/app.js
@@ -1,13 +1,17 @@
-// ********************************************
-// default params
+/**
+ * ********************************************
+ * default params
+ */
 function rollDie(sides = 6) {
   return Math.floor(Math.random() * sides) + 1;
 }
 console.log('roll d4: ' + rollDie(4));
 console.log('roll default (d6): ' + rollDie());
 
-// ********************************************
-// spread
+/**
+ * ********************************************
+ * spread
+ */
 const numbers = [1, 2, 3, 4, 53, 342, 234, 112, 3455];
 
 // this will return NaN because max() expects numbers as arguments
@@ -16,3 +20,10 @@ console.log(Math.max(numbers));
 // this will find the right max, with the spread syntax
 // passes each element in the array as a separate arguments
 console.log(Math.max(...numbers));
+
+// combining using spread
+const cats = ['nero', 'lyanna', 'milo', 'goku'];
+const dogs = ['max', 'bobo'];
+
+const catsAndDogs = [...cats, ...dogs];
+console.log(`cats and dogs: ${catsAndDogs}`);
