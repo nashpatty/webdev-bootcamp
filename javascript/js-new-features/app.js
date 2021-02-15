a1b6f77dfacbc2b4d7cc2dfc803b12479c1ecd25
nashpatty <m.malhan.niazi@gmail.com>
2021-02-15 00:37:03 -0500

    js rest, (not REST)

--- javascript/js-new-features/app.js
+++ javascript/js-new-features/app.js
@@ -16,14 +16,14 @@ const numbers = [1, 2, 3, 4, 53, 342, 234, 112, 3455];
 
 // this will return NaN because max() expects numbers as arguments
 console.log(
-  '🚀 ~ file: app.js ~ line 16 ~ Math.max(numbers):',
+  '📝 ~ file: app.js ~ line 16 ~ Math.max(numbers):',
   Math.max(numbers)
 );
 
 // this will find the right max, with the spread syntax
 // passes each element in the array as a separate arguments
 console.log(
-  '🔧 ¦ file: app.js ¦ line 16 ¦ Math.max(...numbers):',
+  '📝 ~ file: app.js ~ line 16 ~ Math.max(...numbers):',
   Math.max(Math.max(...numbers))
 );
 
@@ -32,11 +32,28 @@ const cats = ['nero', 'lyanna', 'milo', 'goku'];
 const dogs = ['max', 'bobo'];
 
 const catsAndDogs = [...cats, ...dogs];
-console.log('🚀 ~ file: app.js ~ line 29 ~ catsAndDogs:', catsAndDogs);
+console.log('📝 ~ file: app.js ~ line 29 ~ catsAndDogs:', catsAndDogs);
 
 // using spread on objects
 const feline = { legs: 4, family: 'Felidae' };
 const canine = { isFurry: true, family: 'Caninae' };
 
+// common properties will be taken from the second object
+// order matters!
 const newFeline = { ...feline, color: 'various' };
-console.log('🚀 ~ file: app.js ~ line 37 ~ newFeline:', newFeline);
+console.log('📝 ~ file: app.js ~ line 37 ~ newFeline:', newFeline);
+
+/**
+ * ********************************************
+ * rest
+ */
+function noRest(args) {
+  console.log('📝 ~ file: app.js ~ line 51 ~ noRest ~ args', args);
+}
+
+function withRest(...args) {
+  console.log('📝 ~ file: app.js ~ line 55 ~ withRest ~ args', args);
+}
+
+noRest(1, 2, 3, 5); // prints first arg
+withRest(1, 2, 3, 5); // prints all args
