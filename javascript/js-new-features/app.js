64ad8b56b459c55e78b371d2c3f506f7b7eae02a
nashpatty <m.malhan.niazi@gmail.com>
2021-02-14 19:07:48 -0500

    using spread on objects, and enabled turboconsolelog

--- javascript/js-new-features/app.js
+++ javascript/js-new-features/app.js
@@ -15,15 +15,28 @@ console.log('roll default (d6): ' + rollDie());
 const numbers = [1, 2, 3, 4, 53, 342, 234, 112, 3455];
 
 // this will return NaN because max() expects numbers as arguments
-console.log(Math.max(numbers));
+console.log(
+  '🚀 ~ file: app.js ~ line 16 ~ Math.max(numbers):',
+  Math.max(numbers)
+);
 
 // this will find the right max, with the spread syntax
 // passes each element in the array as a separate arguments
-console.log(Math.max(...numbers));
+console.log(
+  '🚀 ~ file: app.js ~ line 16 ~ Math.max(...numbers):',
+  Math.max(Math.max(...numbers))
+);
 
 // combining using spread
 const cats = ['nero', 'lyanna', 'milo', 'goku'];
 const dogs = ['max', 'bobo'];
 
 const catsAndDogs = [...cats, ...dogs];
-console.log(`cats and dogs: ${catsAndDogs}`);
+console.log('🚀 ~ file: app.js ~ line 29 ~ catsAndDogs:', catsAndDogs);
+
+// using spread on objects
+const feline = { legs: 4, family: 'Felidae' };
+const canine = { isFurry: true, family: 'Caninae' };
+
+const newFeline = { ...feline, color: 'various' };
+console.log('🚀 ~ file: app.js ~ line 37 ~ newFeline:', newFeline);
