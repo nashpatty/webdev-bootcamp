3ccc63c6e5d1ce535612dab297408920723bf745
nashpatty <m.malhan.niazi@gmail.com>
2021-05-09 19:10:54 -0400

    high order functions

--- javascript/revisit/functions/app.js
+++ javascript/revisit/functions/app.js
@@ -1,3 +1,23 @@
-let variable1 = 0;
+// passing functions into other functions
+function callTwice(func) {
+  func();
+  func();
+}
 
-console.log('text ' + variable1);
+function rollDie() {
+  let roll = Math.floor(Math.random() * 6) + 1;
+  console.log(roll);
+}
+
+callTwice(rollDie);
+
+// returning functions from other functions
+// creating 'factory' functions
+function makeBetweenFunc(min, max) {
+  return function (num) {
+    return num >= min && num <= max;
+  };
+}
+
+const isChild = makeBetweenFunc(0, 18);
+console.log(isChild(7));
