d9d2ed24ebe616f5f927490a7f0770acadfa6d82
nashpatty <m.malhan.niazi@gmail.com>
2021-05-17 23:00:24 -0400

    testing appending elements

--- js-dom/Manipulating/app.js
+++ js-dom/Manipulating/app.js
@@ -9,3 +9,12 @@ for (let link of allLinks) {
   link.style.textDecorationColor = 'magenta';
   link.style.textDecorationStyle = 'wavy';
 }
+
+const paragraph = document.querySelector('p');
+
+const heading = document.createElement('h1');
+heading.append('Appended Text');
+// this second version doesn't work cuz its not an object
+// heading.appendChild('<h2>second text</h2>');
+
+paragraph.prepend(heading);
