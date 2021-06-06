ace70de75b3f156b174f86b4c06f920f41500187
nashpatty <m.malhan.niazi@gmail.com>
2021-06-06 15:34:28 -0400

    added a color changer button for practice

--- events/app.js
+++ events/app.js
@@ -29,3 +29,32 @@ rblYell.onclick = yell;
 // will print both rebel and yell
 rblYell.addEventListener('click', rebel);
 rblYell.addEventListener('click', yell);
+
+/**
+ * color changer
+ */
+function randomRgbValue() {
+  return Math.floor(Math.random() * (255 - 0) + 0);
+}
+
+// use a light font on darker backgrounds
+// taken from this post: https://stackoverflow.com/questions/3942878/how-to-decide-font-color-in-white-or-black-depending-on-background-color
+function useLightFont(r, g, b) {
+  return r * 0.299 + g * 0.587 + b * 0.114 <= 150;
+}
+
+const colorChangerBtn = document.querySelector('#change-color-btn');
+const rgbDomText = document.querySelector('#rgb-value');
+colorChangerBtn.addEventListener('click', () => {
+  let red = randomRgbValue();
+  let green = randomRgbValue();
+  let blue = randomRgbValue();
+  let rgbString = `rgb(${red}, ${green}, ${blue})`;
+  document.body.style.backgroundColor = rgbString;
+  rgbDomText.innerText = rgbString;
+  if (useLightFont(red, green, blue)) {
+    document.body.style.color = 'white';
+  } else {
+    document.body.style.color = 'black';
+  }
+});
