11137b57c7846271aaa927ea65e8d2fb97748c77
nashpatty <m.malhan.niazi@gmail.com>
2021-06-20 16:27:20 -0400

    promises syntax and chained promises

--- async/app.js
+++ async/app.js
@@ -2,7 +2,7 @@ const delayedColorChange = (color, delay, nextColor) => {
   setTimeout(() => {
     document.body.style.backgroundColor = color;
     // cool trick for null checks
-    nextColor() && nextColor();
+    nextColor && nextColor();
   }, delay);
 };
 
@@ -17,3 +17,34 @@ delayedColorChange('#81b29a', 1000, () => {
     });
   });
 });
+
+// defining a promise
+const dummyPromise = (url) => {
+  return new Promise((resolve, reject) => {
+    const delay = Math.floor(Math.random() * 4500) + 500;
+    setTimeout(() => {
+      if (delay > 4000) {
+        reject('connection timed out');
+      } else {
+        resolve(`data ${url}`);
+      }
+    }, delay);
+  });
+};
+
+// promises
+// chained promises
+dummyPromise('someapi.com/page1')
+  .then(() => {
+    console.log('page 1 recieved');
+    dummyPromise('someapi.com/page2')
+      .then(() => {
+        console.log('page 2 recieved');
+      })
+      .catch(() => {
+        console.log('error page 2');
+      });
+  })
+  .catch(() => {
+    console.log('error page 1');
+  });
